46b0551fc82daf1d2922377a5dd43ddcaea4c12c
HittyGubby <70501834+HittyGubby@users.noreply.github.com>
2023-12-23 13:59:52 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+class Program
+{
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+    [DllImport("user32.dll")]
+    static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+    [DllImport("user32.dll")]
+    static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+    [DllImport("user32.dll")]
+    static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    [DllImport("user32.dll")]
+    static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+    const int GWL_EXSTYLE = -20;
+    const int WS_EX_NOACTIVATE = 0x00000008;
+    const int HWND_TOPMOST = -1;
+    const int SWP_HIDEWINDOW = 0x0080;
+
+    static void Main()
+        {
+            IntPtr hWnd = FindWindow(null, "Sticky Notes"); //find window
+            if (hWnd == IntPtr.Zero) //nonexistent
+            {
+                {
+                    ProcessStartInfo startInfo = new ProcessStartInfo(@"C:\Windows\Sysnative\StikyNot.exe");
+                    Process.Start(startInfo); //start stickynotes
+                    Thread.Sleep(100);
+                    hWnd = FindWindow(null, "Sticky Notes"); //find window again
+                }
+                int style = GetWindowLong(hWnd, GWL_EXSTYLE);
+                style |= WS_EX_NOACTIVATE;
+                SetWindowLong(hWnd, GWL_EXSTYLE, style);
+                SetWindowPos(hWnd, new IntPtr(HWND_TOPMOST), 0, 0, 0, 0, SWP_HIDEWINDOW);
+            }
+            else
+            {
+                int style = GetWindowLong(hWnd, GWL_EXSTYLE);
+                style |= WS_EX_NOACTIVATE; //set window style
+                SetWindowLong(hWnd, GWL_EXSTYLE, style);
+                SetWindowPos(hWnd, new IntPtr(HWND_TOPMOST), 0, 0, 0, 0, SWP_HIDEWINDOW); //make top
+            }
+        }
+    }
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sticky Notes Improved")]
+[assembly: AssemblyDescription("Sticky Notes isn't sticked to the top and showing on the taskbar makes me wanna close it :(")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HittyGubby")]
+[assembly: AssemblyProduct("Sticky Notes Improved")]
+[assembly: AssemblyCopyright("GPL3 License")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("420f2eb6-f615-4e16-8558-a32cbd55e2dc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0")]
+[assembly: AssemblyFileVersion("0.0")]
